4fc0d663a08bfa018351f4ce1b840d4bf75e2128
Brizar <1500595+bmmtstb@users.noreply.github.com>
2023-11-17 18:29:27 +0100

    Delete cleanup.sh

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-pylint -j 0 . &&
- black . &&
-# cd docs && make clean && rm -rf _autosummary && sphinx-build . _build/ -a -j auto && cd ..
- cd docs && make clean && sphinx-build . _build/ -E -a -j auto && cd ..
