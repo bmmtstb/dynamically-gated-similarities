bf1a9edbbf79c76bef60ad356f102ac754c5b936
Martin <1500595+bmmtstb@users.noreply.github.com>
2023-11-17 18:50:00 +0100

    Requirements for Docs and Tests, Smaller env for CI
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- tests/helper.py
+++ tests/helper.py
@@ -1,20 +1,26 @@
 """
 helper methods for tests
 """
+import torch
+
 from dgs.utils.types import Device
 
 
 def test_multiple_devices(func: callable) -> callable:
     """
-    Decorator to run given test on all devices
+    Decorator to run given test on all devices.
+    Check whether CUDA is available and if not use CPU only.
 
     Args:
-        func: the decorated function
+        func: The decorated function
 
     Returns:
         Decorated function with additional device argument
     """
-    devices: list[Device] = ["cpu", "cuda"]
+    if torch.cuda.is_available():
+        devices: list[Device] = ["cpu", "cuda"]
+    else:
+        devices: list[Device] = ["cpu"]
 
     def device_wrapper(cls, *args, **kwargs):
         """wrapper for decorator"""
