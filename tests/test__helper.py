55bb0383c31ec509ae5a0304331d322444485d8e
Martin <1500595+bmmtstb@users.noreply.github.com>
2023-12-17 14:46:01 +0100

    Add test for load_test_image
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- tests/test__helper.py
+++ tests/test__helper.py
@@ -1,7 +1,7 @@
 import unittest
 
 from dgs.utils.types import Device
-from tests.helper import test_multiple_devices
+from tests.helper import load_test_image, test_multiple_devices
 
 
 class TestTestHelpers(unittest.TestCase):
@@ -12,6 +12,21 @@ class TestTestHelpers(unittest.TestCase):
     def test_test_multiple_devices(self, device):
         self.assertEqual(device, self.devices[self.device_id])
         self.device_id += 1
+
+    def test_load_test_image(self):
+        for w, h in [
+            (200, 300),
+            (200, 800),
+            (300, 200),
+            (500, 500),
+            (500, 1000),
+            (800, 200),
+            (1000, 500),
+            (1000, 1000),
+        ]:
+            with self.subTest(msg=f"w,h"):
+                fn: str = f"866-{w}x{h}.jpg"
+                self.assertEqual(load_test_image(fn).shape, (1, 3, h, w))
 
 
 if __name__ == "__main__":
