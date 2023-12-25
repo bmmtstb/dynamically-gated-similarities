a37324f050e3977bc7d288dc0afdfb8fe91017d2
Martin <1500595+bmmtstb@users.noreply.github.com>
2023-12-25 01:08:11 +0100

    Added further tests for files.py
    split is_file() and is_dir() into is_project_X() and is_abs_X()
    tested is_abs_X()
    cleaned up to_abspath even further
    
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- dgs/utils/files.py
+++ dgs/utils/files.py
@@ -19,14 +19,24 @@ def is_project_dir(filepath: FilePath) -> bool:
     return os.path.isdir(os.path.join(PROJECT_ROOT, filepath))
 
 
+def is_abs_file(filepath: FilePath) -> bool:
+    """Returns whether this filepath is an existing files absolute path"""
+    return os.path.isfile(filepath) and os.path.isabs(filepath)
+
+
+def is_abs_dir(filepath: FilePath) -> bool:
+    """Returns whether this filepath is an existing absolute directory"""
+    return os.path.isdir(filepath) and os.path.isabs(filepath)
+
+
 def is_file(filepath: FilePath) -> bool:
     """Returns whether this filepath is an existing file either everywhere or from this project"""
-    return os.path.isfile(filepath) or is_project_file(filepath)
+    return is_abs_file(filepath) or is_project_file(filepath)
 
 
 def is_dir(filepath: FilePath) -> bool:
     """Returns whether this filepath is an existing directory either everywhere or from this project"""
-    return os.path.isdir(filepath) or is_project_dir(filepath)
+    return is_abs_dir(filepath) or is_project_dir(filepath)
 
 
 def to_abspath(filepath: FilePath) -> FilePath:
@@ -42,16 +52,14 @@ def to_abspath(filepath: FilePath) -> FilePath:
     Raises:
         InvalidPathException if the path doesn't exist globally or locally.
     """
-    if os.path.isfile(filepath) or os.path.isdir(filepath):
-        if os.path.isabs(filepath):
-            return os.path.normpath(filepath)
-        return os.path.abspath(filepath)
+    if is_abs_file(filepath) or is_abs_dir(filepath):
+        return os.path.normpath(filepath)
     if is_project_file(filepath) or is_project_dir(filepath):
         return os.path.normpath(os.path.join(PROJECT_ROOT, filepath))
     raise InvalidPathException(f"{filepath} is neither local nor global path.")
 
 
-def mkdir_if_missing(dirname: FilePath) -> None:
+def mkdir_if_missing(dirname: FilePath) -> None:  # pragma: no cover
     """Creates leaf directory and all intermediates if it is missing."""
     if not os.path.exists(dirname):
         os.makedirs(dirname, exist_ok=True)
@@ -78,7 +86,7 @@ def read_json(filepath: FilePath) -> dict[any, any] | list[any]:
     return obj
 
 
-def write_json(obj: dict, filepath: FilePath):
+def write_json(obj: dict, filepath: FilePath):  # pragma: no cover
     """Writes to a json file."""
     fpath = to_abspath(filepath)
     mkdir_if_missing(os.path.dirname(fpath))
--- tests/utils/test__files.py
+++ tests/utils/test__files.py
@@ -3,7 +3,16 @@ import unittest
 
 from dgs.utils.constants import PROJECT_ROOT
 from dgs.utils.exceptions import InvalidPathException
-from dgs.utils.files import is_dir, is_file, is_project_dir, is_project_file, read_json, to_abspath
+from dgs.utils.files import (
+    is_abs_dir,
+    is_abs_file,
+    is_dir,
+    is_file,
+    is_project_dir,
+    is_project_file,
+    read_json,
+    to_abspath,
+)
 from dgs.utils.types import FilePath
 
 
@@ -33,13 +42,41 @@ class TestFiles(unittest.TestCase):
             (prepend_base(r"./tests/test_data/"), True),  # valid directory in abspath, which join ignores
             (prepend_base(r"./tests/dummy/"), False),  # invalid directory
             (r"", True),  # empty string is a valid local path
+            (r".", True),  # this is a valid local path
             (r"./test_data/866-200x300.jpg", False),  # missing tests folder
         ]:
             with self.subTest(msg=f"fp: {fp}, is: {is_pd}"):
                 self.assertEqual(is_project_dir(fp), is_pd)
 
-    def test_is_file(self):
+    def test_is_abs_file(self):
         for fp, is_af in [
+            (r"./tests/utils/test__files.py", False),  # this files local path
+            (r"./tests/test_data/866-200x300.jpg", False),
+            (prepend_base(r"./tests/utils/test__files.py"), True),  # this files local path
+            (prepend_base(r"./tests/test_data/866-200x300.jpg"), True),
+            (prepend_base(r"./tests/test_data/"), False),  # directory not path
+            (prepend_base(r"./test_data/866-200x300.jpg"), False),  # missing tests folder
+            (r"", False),  # empty string
+        ]:
+            with self.subTest(msg=f"fp: {fp}, is: {is_af}"):
+                self.assertEqual(is_abs_file(fp), is_af)
+
+    def test_is_abs_dir(self):
+        for fp, is_ad in [
+            (r"./tests/utils/test__files.py", False),  # this is a file
+            (r"./tests/test_data/", False),  # local directory
+            (prepend_base(r"./tests/utils/test__files.py"), False),  # absolute file
+            (prepend_base(r"./tests/test_data/"), True),  # absolute directory
+            (prepend_base(r"./test_data/"), False),  # absolute but missing tests folder
+            (r"", False),  # empty string is not an absolute path
+            (r".", False),  # valid dir but not valid absolute path
+            (prepend_base(r""), True),  # project root should be a valid absolute path
+        ]:
+            with self.subTest(msg=f"fp: {fp}, is: {is_ad}"):
+                self.assertEqual(is_abs_dir(fp), is_ad)
+
+    def test_is_file(self):
+        for fp, is_f in [
             (r"./tests/utils/test__files.py", True),  # this files local path
             (r"./tests/test_data/866-200x300.jpg", True),
             (prepend_base(r"./tests/utils/test__files.py"), True),  # this files local path
@@ -48,11 +85,11 @@ class TestFiles(unittest.TestCase):
             (prepend_base(r"./test_data/866-200x300.jpg"), False),  # missing tests folder
             (r"", False),  # empty string
         ]:
-            with self.subTest(msg=f"fp: {fp}, is: {is_af}"):
-                self.assertEqual(is_file(fp), is_af)
+            with self.subTest(msg=f"fp: {fp}, is: {is_f}"):
+                self.assertEqual(is_file(fp), is_f)
 
     def test_is_dir(self):
-        for fp, is_ad in [
+        for fp, is_d in [
             (r"./tests/utils/test__files.py", False),  # this is a file
             (prepend_base(r"./tests/test_data/866-200x300.jpg"), False),  # absolute file
             (prepend_base(r"./tests/test_data/"), True),  # absolute directory
@@ -60,8 +97,8 @@ class TestFiles(unittest.TestCase):
             (prepend_base(r"./test_data/"), False),  # absolute but missing tests folder
             (r"", True),  # empty string is a valid local path
         ]:
-            with self.subTest(msg=f"fp: {fp}, is: {is_ad}"):
-                self.assertEqual(is_dir(fp), is_ad)
+            with self.subTest(msg=f"fp: {fp}, is: {is_d}"):
+                self.assertEqual(is_dir(fp), is_d)
 
     def test_read_json(self):
         path = prepend_base(r"./tests/test_data/test.json")
@@ -69,9 +106,10 @@ class TestFiles(unittest.TestCase):
         self.assertEqual(read_json(path), result)
 
     def test_read_json_invalid_path(self):
-        path = prepend_base(r"./tests/test_data/faulty.json")
-        with self.assertRaises(InvalidPathException):
-            read_json(path)
+        for path in [prepend_base(r"./tests/test_data/faulty.json"), prepend_base(r"./tests/test_data/json.faulty")]:
+            with self.subTest(msg=f"path: {path}"):
+                with self.assertRaises(InvalidPathException):
+                    read_json(path)
 
     def test_to_abspath(self):
         for path, result, raises in [
@@ -85,6 +123,16 @@ class TestFiles(unittest.TestCase):
             (prepend_base(r"./utils/"), ..., True),  # tests folder missing
             (r"./tests/utils/invalid.py", ..., True),  # invalid file
             (prepend_base(r"./tests/utils/invalid.py"), ..., True),  # invalid file
+            (  # clean dirty path
+                r"./tests/../tests/./utils/././test__files.py",
+                prepend_base(r"./tests/utils/test__files.py"),
+                False,
+            ),
+            (  # clean dirty path
+                prepend_base(r"./tests/../tests/./utils/././test__files.py"),
+                prepend_base(r"./tests/utils/test__files.py"),
+                False,
+            ),
         ]:
             with self.subTest(msg=f"path: {path}, result: {result}"):
                 if raises:
