a284ca925d35e178502bff498573bba272272b38
Martin <1500595+bmmtstb@users.noreply.github.com>
2023-12-31 17:03:18 +0100

    torchreid needs different imports depending on the way it is installed
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- dgs/models/embedding_generator/torchreid.py
+++ dgs/models/embedding_generator/torchreid.py
@@ -3,14 +3,26 @@ Visual Re-ID module using the torchreid package.
 """
 import torch
 from torch import nn
-from torchreid.reid import models
-from torchreid.reid.models import __model_factory as torchreid_models
-from torchreid.reid.utils.torchtools import load_pretrained_weights
 from torchvision.transforms.v2.functional import to_dtype
 
 from dgs.models.embedding_generator.embedding_generator import EmbeddingGeneratorModule
 from dgs.utils.files import to_abspath
 from dgs.utils.types import Config
+
+try:
+    # If torchreid is installed using `./dependencies/torchreid`
+    # noinspection PyUnresolvedReferences LongLine
+    from torchreid.models import __model_factory as torchreid_models, build_model
+
+    # noinspection PyUnresolvedReferences LongLine
+    from torchreid.utils.torchtools import load_pretrained_weights
+except ModuleNotFoundError:
+    # if torchreid is installed using `pip install torchreid`
+    # noinspection PyUnresolvedReferences
+    from torchreid.reid.models import __model_factory as torchreid_models, build_model
+
+    # noinspection PyUnresolvedReferences
+    from torchreid.reid.utils.torchtools import load_pretrained_weights
 
 torchreid_validations: Config = {"model_name": ["str", ("in", torchreid_models.keys())]}
 
@@ -30,7 +42,7 @@ class TorchreidModel(EmbeddingGeneratorModule):
 
     def _init_model(self) -> nn.Module:
         """Initialize torchreid model"""
-        m = models.build_model(
+        m = build_model(
             name=self.params["model_name"],
             num_classes=self.embedding_size,
             pretrained=self.is_pretrained,
