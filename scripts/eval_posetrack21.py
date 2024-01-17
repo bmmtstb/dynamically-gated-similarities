01a94c7f6ff7b83a68778999791efa262813a7c0
Martin <1500595+bmmtstb@users.noreply.github.com>
2024-01-17 14:53:05 +0100

    Fixed pylint warning
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- scripts/eval_posetrack21.py
+++ scripts/eval_posetrack21.py
@@ -6,11 +6,11 @@ import posetrack21.api as pt21_eval_api
 from dgs.utils.files import to_abspath
 
 if __name__ == "__main__":
-    eval_type = "tracking"
+    EVAL_TYPE = "tracking"
     evaluator = pt21_eval_api.get_api(
         trackers_folder=to_abspath("./data/PoseTrack21/...predictions..."),
         gt_folder=to_abspath("./data/PoseTrack21/posetrack_data/annotations/val/"),
-        eval_type=eval_type,
+        eval_type=EVAL_TYPE,
         num_parallel_cores=8,
         use_parallel=True,
     )
