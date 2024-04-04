fe7df0c0b998b387e475d01cbd1d29bfab596abf
Martin <1500595+bmmtstb@users.noreply.github.com>
2024-04-04 11:54:18 +0200

    KeypointRCNNBackbone added score threshold
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- dgs/models/backbone/keypoint_rcnn.py
+++ dgs/models/backbone/keypoint_rcnn.py
@@ -12,9 +12,12 @@ from torchvision import tv_tensors as tvte
 from torchvision.models.detection import keypointrcnn_resnet50_fpn, KeypointRCNN_ResNet50_FPN_Weights
 
 from dgs.models.backbone.backbone import BaseBackboneModule
+from dgs.utils.config import DEF_CONF
 from dgs.utils.image import load_image_list
 from dgs.utils.state import collate_states, State
-from dgs.utils.types import Config, FilePath, FilePaths, NodePath
+from dgs.utils.types import Config, FilePath, FilePaths, NodePath, Validations
+
+rcnn_validations: Validations = {"threshold": ["optional", float, ("within", (0.0, 1.0))]}
 
 
 class KeypointRCNNBackbone(BaseBackboneModule):
@@ -23,10 +26,20 @@ class KeypointRCNNBackbone(BaseBackboneModule):
 
     References:
         https://pytorch.org/vision/0.17/models/generated/torchvision.models.detection.keypointrcnn_resnet50_fpn.html#torchvision.models.detection.keypointrcnn_resnet50_fpn
+
+    Params
+    ------
+
+    threshold (float):
+        Detections with a score lower than the threshold will be ignored.
+        Default `DEF_CONF.backbone.kprcnn.threshold`.
     """
 
     def __init__(self, config: Config, path: NodePath) -> None:
         super().__init__(config, path)
+        self.validate_params(rcnn_validations)
+
+        self.threshold: float = self.params.get("threshold", DEF_CONF.backbone.kprcnn.threshold)
 
         model = keypointrcnn_resnet50_fpn(weights=KeypointRCNN_ResNet50_FPN_Weights.COCO_V1, progress=True)
         model.eval()
@@ -35,6 +48,8 @@ class KeypointRCNNBackbone(BaseBackboneModule):
 
     def forward(self, image_paths: Union[FilePath, FilePaths], *args, **kwargs) -> State:
         """Given a tuple of image paths, return a State containing the bounding boxes and the predicted key-points."""
+        if isinstance(image_paths, str):
+            image_paths = (image_paths,)
         assert len(image_paths) > 0, "No images provided"
 
         images = load_image_list(filepath=image_paths, dtype=torch.float32, device=self.device)
@@ -44,11 +59,14 @@ class KeypointRCNNBackbone(BaseBackboneModule):
         canvas_size = (max(i.shape[-2] for i in images), max(i.shape[-1] for i in images))
 
         for output, path in zip(outputs, image_paths):
+            # for every image (output), get the indices where score is bigger than the threshold
+            idx = torch.where(output["scores"] > self.threshold)
+
             # bbox given in XYXY format
-            bbox = tvte.BoundingBoxes(output["boxes"], format="XYXY", canvas_size=canvas_size)
+            bbox = tvte.BoundingBoxes(output["boxes"][idx], format="XYXY", canvas_size=canvas_size)
             # keypoints in [x,y,v] format -> kp, vis
             kp, vis = (
-                output["keypoints"]
+                output["keypoints"][idx]
                 .to(device=self.device, dtype=self.precision)
                 .reshape((-1, 17, 3))
                 .split([2, 1], dim=-1)
--- tests/models/backbone/test__keypoint_rcnn.py
+++ tests/models/backbone/test__keypoint_rcnn.py
@@ -6,6 +6,7 @@ from torch.nn import Module as TorchModule
 
 from dgs.models.backbone.backbone import BaseBackboneModule
 from dgs.models.backbone.keypoint_rcnn import KeypointRCNNBackbone
+from dgs.utils.config import fill_in_defaults
 from dgs.utils.state import State
 from helper import get_test_config
 
@@ -20,10 +21,11 @@ class TestKPRCNNModel(unittest.TestCase):
         self.assertTrue(isinstance(m, BaseBackboneModule))
         self.assertTrue(isinstance(m.model, TorchModule))
         self.assertFalse(m.model.training)
+        self.assertTrue(m.threshold > 0.0)
 
     def test_forward(self):
-        cfg = get_test_config()
-        m = KeypointRCNNBackbone(config=cfg, path=[])
+        cfg = fill_in_defaults({"kprcnn": {"threshold": 0.0}}, get_test_config())
+        m = KeypointRCNNBackbone(config=cfg, path=["kprcnn"])
 
         for fp, B in [
             (IMAGE_PATH, 2),
@@ -36,6 +38,15 @@ class TestKPRCNNModel(unittest.TestCase):
                 self.assertFalse(out.validate)
                 self.assertEqual(out.keypoints.shape, torch.Size((B, 17, 2)))
                 self.assertEqual(out.joint_weight.shape, torch.Size((B, 17, 1)))
+                self.assertEqual(len(out.filepath), B)
+
+    def test_forward_with_threshold(self):
+        cfg = fill_in_defaults({"kprcnn": {"threshold": 0.5}}, get_test_config())
+
+        m = KeypointRCNNBackbone(config=cfg, path=["kprcnn"])
+        out = m(IMAGE_PATH)
+        self.assertTrue(isinstance(out, State))
+        self.assertEqual(out.B, 1)
 
 
 if __name__ == "__main__":
