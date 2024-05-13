109498f7c3efbba144d6c4c3e3aa0752a4b7fdc1
Martin <1500595+bmmtstb@users.noreply.github.com>
2024-05-13 02:34:39 +0200

    Work on eval_single_similarity.py
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- scripts/own/eval_single_similarity.py
+++ scripts/own/eval_single_similarity.py
@@ -17,6 +17,7 @@ from dgs.models.engine.dgs_engine import DGSEngine
 from dgs.models.loader import module_loader
 from dgs.utils.config import load_config
 from dgs.utils.torchtools import close_all_layers, memory_analysis
+from dgs.utils.types import Config
 from dgs.utils.utils import HidePrint
 
 CONFIG_FILE = "./configs/DGS/eval_sim_indep.yaml"
@@ -24,15 +25,16 @@ CONFIG_FILE = "./configs/DGS/eval_sim_indep.yaml"
 
 @memory_analysis
 @torch.no_grad()
-def run(dl_key: str, paths: list[str]) -> None:
+def run(config: Config, dl_key: str, paths: list[str]) -> None:
     """Main function to run the code."""
-    # get sub folders or files and analyse them one-by-one
-    for sub_datapath in tqdm(paths, desc="ds_sub_dir"):
+    # IoU, OKS, and visual similarity
+    for dgs_key in tqdm(["dgs_box", "dgs_pose", "dgs_vis_3", "dgs_vis_2", "dgs_vis_3"], desc="similarities"):
+        # for dgs_key in tqdm(["dgs_vis_3", "dgs_vis_2", "dgs_vis_3"], desc="similarities"):
 
-        config[dl_key]["data_path"] = sub_datapath
+        # get sub folders or files and analyse them one-by-one
+        for sub_datapath in tqdm(paths, desc="ds_sub_dir"):
 
-        # IoU, OKS, and visual similarity
-        for dgs_key in tqdm(["dgs_box", "dgs_pose", "dgs_vis_3", "dgs_vis_2", "dgs_vis_3"], desc="similarities"):
+            config[dl_key]["data_path"] = sub_datapath
 
             # make sure to have a unique log dir every time
             orig_log_dir = config["log_dir"]
@@ -57,15 +59,14 @@ def run(dl_key: str, paths: list[str]) -> None:
 if __name__ == "__main__":
     print(f"Cuda available: {torch.cuda.is_available()}")
 
-    print(f"Loading configuration: {CONFIG_FILE}")
-    config = load_config(CONFIG_FILE)
-
     print("Evaluating on the PT21 ground-truth evaluation dataset")
-    base_path = config["dl_gt"]["base_path"]
+    cfg = load_config(CONFIG_FILE)
+    base_path = cfg["dl_gt"]["base_path"]
     data_paths = [f.path for f in os.scandir(base_path) if f.is_file()]
-    run(dl_key="dl_gt", paths=data_paths)
+    run(config=cfg, dl_key="dl_gt", paths=data_paths)
 
     print("Evaluating on the PT21 eval-dataset using KeypointRCNN as prediction backbone")
-    base_path = config["dl_rcnn"]["base_path"]
+    cfg = load_config(CONFIG_FILE)
+    base_path = cfg["dl_rcnn"]["base_path"]
     data_paths = [f.path for f in os.scandir(base_path) if f.is_dir()]
-    run(dl_key="dl_rcnn", paths=data_paths)
+    run(config=cfg, dl_key="dl_rcnn", paths=data_paths)
