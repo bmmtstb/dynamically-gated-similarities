cd382435865609b2b930acd1e85264f6b7ee1533
Martin <1500595+bmmtstb@users.noreply.github.com>
2024-11-24 19:08:24 +0100

    added script to predict using dynamic weights
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

---
+++ scripts/own/predict_using_dynamic_weights.py
@@ -0,0 +1,47 @@
+"""Use the (trained) DGS module to track / predict a video input."""
+
+import time
+from datetime import timedelta
+
+import torch as t
+
+from dgs.models.engine.dgs_engine import DGSEngine
+from dgs.models.loader import module_loader
+from dgs.utils.config import load_config
+
+CONFIG_FILE = "./configs/DGS/predict_images_trained.yaml"
+DL = "dataloader_dance"
+SUBMISSION = ["submission_MOT"]  # or "submission_pt21"
+ALPHA_MODULES = ["box_fc2_2Sigmoid", "pose_coco_fc2_2Sigmoid", "visual_osn_fc3_3Sigmoid"]
+
+if __name__ == "__main__":
+    print(f"Loading configuration: {CONFIG_FILE}")
+    config = load_config(CONFIG_FILE)
+    print(f"Cuda available: {t.cuda.is_available()}")
+
+    # modify config
+    config["test"]["submission"] = SUBMISSION
+    config["dynamic_alpha_combine"]["alpha_modules"] = ALPHA_MODULES
+
+    # validation dataset
+    print("Loading data")
+    ds_start_time = time.time()
+    test_dl = module_loader(config=config, module_type="dataloader", key=DL)
+    print(f"Total data loading time: {str(timedelta(seconds=round(time.time() - ds_start_time)))}")
+
+    module_start_time = time.time()
+
+    engine = DGSEngine(config=config, path=["engine"], test_loader=test_dl)
+    engine.model.eval()
+
+    engine.predict()
+
+    print("Combine images to video")
+    print("Use ffmpeg, because it is faster and more stable. Run:")
+    print(f"cd {engine.log_dir}")
+    print("ffmpeg -framerate 30 -pattern_type glob -i './images/*.png' prediction.mp4")
+    print("----")
+    print(
+        "Or use function 'combine_images_to_video' as commented out in 'scripts/predict.py', "
+        "but this function is way slower."
+    )
